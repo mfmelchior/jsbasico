307fef6b6611fe848567182d517899f161e195c8
Fernanda <mfmelchiorsantarosa@gmail.com>
2023-12-01 14:38:34 -0300

    first commit

---
+++ funcionalidade.js
@@ -0,0 +1,7 @@
+var copo = window.document.getElementById("copo") //criando uma variável que chama copo
+//e essa variável ta colocando dentro dela uma informação que ta dentro da minha página no meu documento que tem o id copo
+function encher_copo (){
+    //criando uma funcionalidade, o que a funcionalidade precisa fazer deve estar dentro das chaves
+    copo.src = "copocheio.svg" // quando eu acionar essa função ele vai mostrar essa imagem
+}
+// para a função ser executada meidnate algum evento, nesse caso, o ebento on click (no momento que clicar)
